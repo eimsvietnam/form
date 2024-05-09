8e4c9aa0b8233833dabd6c6b9bc467004e83889c
Nguyễn Trung Nghĩa <87295854+trngbro@users.noreply.github.com>
2024-05-09 15:22:45 +0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ import {
   Box,
   Image,
   useDisclosure,
-  AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay 
+  AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay, 
+  Center
 } from '@chakra-ui/react';
 
 function App() {
@@ -46,7 +47,7 @@ function App() {
   useEffect(() => {
     async function fetchExhibitions() {
       try {
-        const response = await fetch('https://b2b-n0r5.onrender.com/users/exhibition');
+        const response = await fetch('/users/exhibition');
         const data = await response.json();
         if (data.success) {
           setExhibitions(data.data);
@@ -74,7 +75,13 @@ function App() {
     setUserData((prevData) => ({
       ...prevData,
       [name]: value,
+      refresh: false
     }));
+    if(imageContainer){
+      setImageContainer(JSON.parse(selectedExhibition).cardLink)
+    }
+
+    
   };
 
   const handleRefresh = (e) => {
@@ -88,6 +95,7 @@ function App() {
       phone: '',
       website: '',
       note: '',
+      pic: userData.pic,
       refresh: false
     })
   };
@@ -106,7 +114,7 @@ function App() {
   const handleSubmit = async () => {
     setIsSendingData(true);
     try {
-      const response = await fetch('https://b2b-n0r5.onrender.com/users/', {
+      const response = await fetch('/users/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -172,8 +180,8 @@ function App() {
           </AlertDialogContent>
         </AlertDialogOverlay>
       </AlertDialog>
-      <Grid templateColumns="repeat(2, 1fr)" gap={2}  mt="20">
-        <GridItem w="100%">
+      <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={2}  mt="20">
+        <GridItem w="100%" textAlign="center">
           <VStack>
             <Container maxW="md" bg="white" color="">
               <FormControl>
@@ -297,24 +305,26 @@ function App() {
             </Container>
           </VStack>
         </GridItem>
-        <GridItem>
-          <Box boxSize='sm'>
-          {imageContainer && (
-            <Image src={imageContainer} id='image-container' alt='CARD' />
-          )}
+        <GridItem w="100%" textAlign="center">
+          <Center>
+            <Box boxSize='sm'>
+                {imageContainer && (
+                <Image src={imageContainer} id='image-container' alt='CARD' />
+              )}
 
-          {userData.refresh && (
-            <>
-              <Button mr='10' colorScheme='blue'
-                onClick={handleDownload}
-              >Download</Button>
-              <Button mr='10' colorScheme='yellow'
-                onClick={handleRefresh}
-              >Refresh</Button>
-            </>
-            
-          )}
-          </Box>
+              {userData.refresh && (
+                <>
+                  <Button mr='10' colorScheme='blue'
+                    onClick={handleDownload}
+                  >Download</Button>
+                  <Button mr='10' colorScheme='yellow'
+                    onClick={handleRefresh}
+                  >Refresh</Button>
+                  
+                </>
+              )}
+            </Box>
+          </Center>
         </GridItem>
       </Grid>
     </ChakraProvider>
